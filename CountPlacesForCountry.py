9fc3fd1eb8ee9a1d192247cf780256d9c6f741e5
awbecker25 <53805095+awbecker25@users.noreply.github.com>
2020-05-13 19:52:20 -0400

    Add files via upload

---
+++ CountPlacesForCountry.py
@@ -0,0 +1,22 @@
+﻿import arcpy
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = r"C:\LPA\Projects\FrameworkProject\FrameworkProject.gdb"
+countryName = arcpy.GetParameterAsText(0)
+arcpy.analysis.Select(r"C:\LPA\Data\ne_10m_admin_0_countries.shp",
+                      r"C:\LPA\Projects\FrameworkProject\FrameworkProject.gdb\SelCountry",
+                      f"NAME = '{countryName}'")
+arcpy.analysis.Buffer("SelCountry",
+                      "SelCountry_Buffer",
+                      "200 Kilometers",
+                      "FULL",
+                      "ROUND",
+                      "NONE",
+                      None,
+                      "PLANAR")
+arcpy.analysis.Clip(r"C:\LPA\Data\ne_10m_populated_places.shp",
+                    "SelCountry_Buffer",
+                    "Places_Clip",
+                    None)
+arcpy.AddMessage(f"There are {arcpy.management.GetCount('Places_Clip')} populated places in or within 200km of {countryName}")
+
+
